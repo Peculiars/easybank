751154038e7648bb38c5fd880e723848a0b17e00
Olaitan Damilare <olaitandamilare230@gmail.com>
2024-02-11 12:37:14 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,13 @@ import desktopBg from '../assets/images/bg-intro-desktop.svg';
 import phones from '../assets/images/image-mockups.png'
 
 export const Hero = () => {
-
   return (
     <div className='hero-container'>
         <div className='hero-images'>
-{/*             <div className='mobile-hero-bg' style={mobileHeroBgStyle}></div> */}
           <img className='mobile-hero-bg' src={mobileBg} alt="Mobile background" />
           <img className='desktop-hero-bg' src={desktopBg} alt='Desktop background' />
         </div>
         <img className='hero-phones' src={phones} alt={phones} />
-        
         <div className='hero-contents'>
             <h1 className='hero-heading'>Next generation digital banking</h1>
             <p  className='hero-desc'>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing and much more.</p>
