a85d9b6a6048f1c30b314ad0cafe5eb445a2a241
Olaitan Damilare <olaitandamilare230@gmail.com>
2024-02-11 04:51:25 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,6 @@ import desktopBg from '../assets/images/bg-intro-desktop.svg';
 import phones from '../assets/images/image-mockups.png'
 
 export const Hero = () => {
-  const mobileHeroBgStyle = {
-    backgroundImage: `url(${mobileBg})`,
-    /* Additional background properties */
-  };
-
   const desktopHeroBgStyle = {
     backgroundImage: `url(${desktopBg})`,
     /* Additional background properties */
@@ -17,7 +12,8 @@ export const Hero = () => {
   return (
     <div className='hero-container'>
         <div className='hero-images'>
-            <div className='mobile-hero-bg' style={mobileHeroBgStyle}></div>
+{/*             <div className='mobile-hero-bg' style={mobileHeroBgStyle}></div> */}
+          <img className='mobile-hero-bg' src={mobileBg} alt="Mobile background" />
             <div className='desktop-hero-bg' style={desktopHeroBgStyle}></div>
         </div>
         <img className='hero-phones' src={phones} alt={phones} />
